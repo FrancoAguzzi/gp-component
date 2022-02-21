e2230451246b701628a7b40f1402985aa4c8c5a9
Franco Aguzzi <franco.aguzzi@timeprimo.com>
2022-02-21 15:10:09 -0300

    a

--- collection/lib/validation.js
+++
@@ -1,1 +0,0 @@
-export const isValidEmail = str => /(^[^@][a-z0-9-_.]+)(@{1})([a-z0-9-_]+[^@])(\.{1})([a-z0-9]+[^@])/i.test(str);
--- types/lib/CookieService.d.ts
+++
@@ -1,5 +0,0 @@
-declare const CookieService: {
-  setCookie(name: any, value: any, days: any): void;
-  getCookie(name: any): string;
-};
-export default CookieService;
